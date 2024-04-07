b1f645988cbbb78604f0cd5af4d5a9014bfb4228
Mery Álvarez <159366647+Meryalvhe@users.noreply.github.com>
2024-04-07 15:28:03 +0000

    feat[GeneradorDeCartasAleatorias]: app.js, index.html and styles.css

--- src/app.js
+++ src/app.js
@@ -7,5 +7,75 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#number").innerHTML = cardNumberRandom();
+  let palosuperior = (document.querySelector(
+    ".palo"
+  ).innerHTML = cardPaloRandom());
+  document.querySelector("#paloBotton").innerHTML = palosuperior;
+  click();
+  setInterval(timeOut, 2000);
 };
+
+function cardNumberRandom() {
+  let cardNumber = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let cardNumberAleatory = Math.floor(Math.random() * cardNumber.length);
+
+  return cardNumber[cardNumberAleatory];
+}
+
+function cardPaloRandom() {
+  let cardSimbol = ["♦", "♥", "♠", "♣"];
+
+  let cardPaloAleatory = Math.floor(Math.random() * cardSimbol.length);
+
+  color(cardPaloAleatory);
+
+  return cardSimbol[cardPaloAleatory];
+}
+
+function color(element) {
+  let simbolColor = "";
+  if (element == 0 || element == 1) {
+    simbolColor = "red";
+  } else {
+    simbolColor = "black";
+  }
+
+  let cardTop = document.querySelector(".palo");
+  let cardBotton = document.querySelector("#paloBotton");
+  cardTop.style.color = simbolColor;
+  cardBotton.style.color = simbolColor;
+}
+
+function click() {
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#number").innerHTML = cardNumberRandom();
+    let palosuperior = (document.querySelector(
+      ".palo"
+    ).innerHTML = cardPaloRandom());
+    document.querySelector("#paloBotton").innerHTML = palosuperior;
+  });
+}
+
+function timeOut() {
+  document.querySelector("#number").innerHTML = cardNumberRandom();
+  let palosuperior = (document.querySelector(
+    ".palo"
+  ).innerHTML = cardPaloRandom());
+  document.querySelector("#paloBotton").innerHTML = palosuperior;
+}
